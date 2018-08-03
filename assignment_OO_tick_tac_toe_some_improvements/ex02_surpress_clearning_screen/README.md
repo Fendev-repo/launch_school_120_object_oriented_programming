0f3d2bff0463c0ea92478125f649860b19edb80c
Fen <fendev@protonmail.com>
2018-08-03 12:27:25 +1000

    seperated classes into seperate rb docs to make program more clear and usable

--- assignment_OO_tick_tac_toe_some_improvements/ex02_surpress_clearning_screen/README.md
+++ assignment_OO_tick_tac_toe_some_improvements/ex02_surpress_clearning_screen/README.md
@@ -5,15 +5,8 @@ The first time we display a board, we want to suppress the clearing of the scree
 
 ### Problem decomposition
 	- Sub-problems
-		- where is the best place to define this method?
-			Option 1: Instance method in TTTGame class
-			Option 2: Instance method in Board class
-			I think that its best placed in the TTTGame, as there
-			are other similiar method display_result(). Depending
-			on future requirements, it may be best placed to
-			extract this and other similiar messages to a module.
+		- 
 ### Examples
-	clear() => # clears the terminal
 ### Data structures
 ### Algorithm
 ### Code
---
+++ assignment_OO_tick_tac_toe_some_improvements/ex02_surpress_clearning_screen/lib/interactable_module.rb
@@ -0,0 +1,68 @@
+# Responsible for keeping all the methods that interact with the terminal
+module Interactable
+  def display_welcome_message
+    puts 'Welcome to Tic Tac Toe!'
+    puts ''
+  end
+
+  def display_goodbye_message
+    puts 'Thanks for playing Tic Tac Toe! Goodbye!'
+  end
+
+  def display_board(clear = true)
+    clear_cli if clear
+    puts "You're a #{human.marker}.  Computer is a #{computer.marker}"
+    puts ''
+    puts '      |     |'
+    puts "   #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
+    puts '      |     |'
+    puts '-------------------'
+    puts '      |     |'
+    puts "   #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}"
+    puts '      |     |'
+    puts '-------------------'
+    puts '      |     |'
+    puts "   #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
+    puts '      |     |'
+    puts ''
+  end
+
+  def human_moves
+    puts "Choose a square (#{board.unmarked_keys.join(', ')})"
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if board.unmarked_keys.include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+    board.set_square_at(square, human.marker)
+  end
+
+  def display_result
+    display_board
+
+    case board.detect_winner
+    when human.marker
+      puts 'You won!'
+    when computer.marker
+      puts 'Computer won!'
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def clear_cli
+    system 'clear'
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts 'Would you like to play again? (y/n)'
+      answer = gets.chomp.downcase
+      break if %w(y n).include? answer
+      puts 'Sorry, must be y or n'
+    end
+    answer == 'y'
+  end
+end
